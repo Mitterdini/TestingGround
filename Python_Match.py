54b844ffddd94c48359c77f8f2cf3f74b39bd68d
Matthew Holland <m.b.holland1996@gmail.com>
2022-07-09 17:35:44 -0400

    made it self looping

--- Python_Match.py
+++ Python_Match.py
@@ -1,12 +1,17 @@
 #!/user/bin/python3.10
-
-user_input = int(input("gimme dat number (1-3): \n> "))
-match user_input:
-    case 1:
-        print("loser")
-    case 2:
-        print("uhhh no you")
-    case 3:
-        print("i mean... i guess... baka!")
-    case _:     # this is the default structure
-        print("i said between 1 and 3, IDIOT...")
+def match():
+    user_input = int(input("\n\n\nenter 4 to quit\n\ngimme dat number (1-3): \n> "))
+    match user_input:
+        case 1:
+            print("loser")
+        case 2:
+            print("uhhh no you")
+        case 3:
+            print("i mean... i guess... baka!")
+        case 4:
+            print("\nquitting...")
+            quit()
+        case _:     # this is the default structure
+            print("i said between 1 and 3, IDIOT...")
+    match()
+match()
